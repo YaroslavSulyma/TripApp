0db8e06993fdac8f3261d7c06bc5a23a284c6671
Yaroslav <yaryksulima@gmail.com>
2021-01-25 13:30:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tripapp/CameraFragment.kt
@@ -0,0 +1,22 @@
+package com.example.tripapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CameraFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
