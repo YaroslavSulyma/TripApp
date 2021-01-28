537e7cba85ab54e0544d2b0ae1aa0c56b95f17ec
YaroslavSulyma <yaryksulima@gmail.com>
2021-01-28 12:09:54 +0200

    *Changes in Default Repository Created RepositoryUtil for safeCall

--- app/src/main/java/com/example/tripapp/repositories/DefaultRepository.kt
+++ app/src/main/java/com/example/tripapp/repositories/DefaultRepository.kt
@@ -1,15 +1,34 @@
 package com.example.tripapp.repositories
 
+import com.example.tripapp.data.entities.User
 import com.example.tripapp.utils.Resource
+import com.example.tripapp.utils.safeCall
 import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class DefaultRepository : AuthRepository {
+
+    val auth = FirebaseAuth.getInstance()
+    val users = FirebaseFirestore.getInstance().collection("users")
+
     override suspend fun register(
         email: String,
         username: String,
         password: String
     ): Resource<AuthResult> {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            safeCall {
+                val result = auth.createUserWithEmailAndPassword(email, password).await()
+                val uid = result.user?.uid!!
+                val user = User(uid, username)
+                users.document(uid).set(user).await()
+                Resource.Success(result)
+            }
+        }
     }
 
     override suspend fun login(email: String, password: String): Resource<AuthResult> {
